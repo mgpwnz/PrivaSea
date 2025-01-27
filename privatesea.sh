5bf6fd275df1ba0a48b7ad71f9e2154544f3d4d6
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2025-01-27 20:08:59 +0200

    upd

--- privatesea.sh
+++ privatesea.sh
@@ -1,62 +1,85 @@
 #!/bin/bash
 while true
 do
-
 # Menu
-
 PS3='Select an action: '
 options=("Pre Install" "Create Account" "Install & RUN" "Logs" "Uninstall" "Exit")
 select opt in "${options[@]}"
-               do
-                   case $opt in                          
+do
+    case $opt in
+        "Pre Install")
+            # Install Docker and Docker Compose
+            touch $HOME/.bash_profile
+            cd $HOME
+            if ! command -v docker &> /dev/null; then
+                sudo apt update
+                sudo apt upgrade -y
+                sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
+                . /etc/*-release
+                wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+                echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                sudo apt update
+                sudo apt install docker-ce docker-ce-cli containerd.io -y
+            fi
 
-"Pre Install")
-#docker + compose
-touch $HOME/.bash_profile
-	cd $HOME
-	if ! docker --version; then
-		sudo apt update
-		sudo apt upgrade -y
-		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
-		. /etc/*-release
-		wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-		sudo apt update
-		sudo apt install docker-ce docker-ce-cli containerd.io -y
-		docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
-		sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
-	fi
-	if ! docker compose version; then
-		sudo apt update
-		sudo apt upgrade -y
-		sudo apt install wget jq -y
-		local docker_compose_version=`wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name"`
-		sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
-		sudo chmod +x /usr/bin/docker-compose
-		. $HOME/.bash_profile
-	fi
+            if ! docker compose version &> /dev/null; then
+                sudo apt install wget jq -y
+                docker_compose_version=$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
+                sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-$(uname -s)-$(uname -m)"
+                sudo chmod +x /usr/bin/docker-compose
+            fi
+            echo "Docker and Docker Compose installed successfully."
+            break
+            ;;
 
-break
-;;
+		"Create Account")
+			# Pull and create account
+			if [ -d "$HOME/PrivateSea" ] && [ -f "$HOME/PrivateSea/config/wallet_keystore" ]; then
+				echo "Key already exists at $HOME/PrivateSea/config/wallet_keystore. No need to generate a new one."
+				break
+			fi
 
-"Create Account")
-# Pull
-docker pull privasea/acceleration-node-beta:latest
-sudo mkdir -p $HOME/PrivateSea/config
-cd $HOME/PrivateSea
-docker run -it -v $HOME/PrivateSea/config:/app/config privasea/acceleration-node-beta:latest ./node-calc new_keystore
-KEY_PATH=$(find $HOME/PrivateSea/config/ -type f | head -n 1)
-if [ -z "$KEY_PATH" ]; then
-  echo "Key not found!"
-  exit 1
-fi
-KEY_NAME=$(basename $KEY_PATH)
-mv $HOME/PrivateSea/config/$KEY_NAME $HOME/PrivateSea/config/wallet_keystore
-break
-;;
-"Install & RUN")
-read -p "Enter Password: " Password
-echo 'export Password='${Password}
+			docker pull privasea/acceleration-node-beta:latest
+			sudo mkdir -p $HOME/PrivateSea/config
+			cd $HOME/PrivateSea
+			docker run -it -v $HOME/PrivateSea/config:/app/config privasea/acceleration-node-beta:latest ./node-calc new_keystore
+
+			KEY_PATH=$(find $HOME/PrivateSea/config/ -type f | head -n 1)
+			if [ -z "$KEY_PATH" ]; then
+				echo "Key not found! Please try again."
+			else
+				KEY_NAME=$(basename "$KEY_PATH")
+				mv "$HOME/PrivateSea/config/$KEY_NAME" "$HOME/PrivateSea/config/wallet_keystore"
+				echo "Account created successfully. Key stored as wallet_keystore."
+			fi
+			break
+			;;
+
+
+		"Install & RUN")
+			if [ -f "$HOME/PrivateSea/docker-compose.yml" ]; then
+				echo "docker-compose.yml already exists. Checking container status..."
+
+				# Перевіряємо, чи контейнер запущений
+				if ! docker ps --filter "name=acceleration-node" --format "{{.Names}}" | grep -q "acceleration-node"; then
+					echo "Container is not running. Starting the container..."
+					docker compose -f $HOME/PrivateSea/docker-compose.yml up -d
+				else
+					echo "Container is already running."
+				fi
+
+				docker logs -f acceleration-node --tail 100
+				break
+			fi
+
+			# Якщо файлу docker-compose.yml немає, створюємо його
+			read -p "Enter Password: " Password
+			if [ -z "$Password" ]; then
+				echo "Password cannot be empty. Please try again."
+				break
+			fi
+			echo "export Password=${Password}" >> $HOME/.bash_profile
+			source $HOME/.bash_profile
 tee $HOME/PrivateSea/docker-compose.yml > /dev/null <<EOF
 version: '3.8'
 
@@ -71,37 +94,42 @@ services:
     restart: unless-stopped
 
 EOF
-docker compose -f $HOME/PrivateSea/docker-compose.yml up -d
-docker logs -f acceleration-node --tail 100
-break
-;;
+			docker compose -f $HOME/PrivateSea/docker-compose.yml up -d
+			docker logs -f acceleration-node --tail 100
+			break
+			;;
 
-"Logs")
-docker logs -f acceleration-node --tail 100
-break
-;;
 
-"Uninstall")
-if [ ! -d "$HOME/PrivateSea" ]; then
-    break
-fi
-read -r -p "Remove node? [y/N] " response
-case "$response" in
-    [yY][eE][sS]|[yY]) 
-docker compose -f $HOME/PrivateSea/docker-compose.yml down -v
-        ;;
-    *)
-	echo Canceled
-	break
-        ;;
-esac
-break
-;;
+        "Logs")
+            docker logs -f acceleration-node --tail 100
+            break
+            ;;
 
-"Exit")
-exit
-;;
-*) echo "invalid option $REPLY";;
-esac
+        "Uninstall")
+            if [ ! -d "$HOME/PrivateSea" ]; then
+                echo "No installation found."
+                break
+            fi
+            read -r -p "Remove node? [y/N] " response
+            case "$response" in
+                [yY][eE][sS]|[yY]) 
+                    docker compose -f $HOME/PrivateSea/docker-compose.yml down -v
+                    echo "Node removed successfully."
+                    ;;
+                *)
+                    echo "Canceled"
+                    ;;
+            esac
+            break
+            ;;
+
+        "Exit")
+            exit
+            ;;
+
+        *)
+            echo "Invalid option $REPLY"
+            ;;
+    esac
 done
 done
