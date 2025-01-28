719c188d2349f2e271eb2131b7c61ae0ed8bfa30
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2025-01-28 20:56:51 +0200

    upd

--- privasea.sh
+++ privasea.sh
@@ -8,29 +8,30 @@ select opt in "${options[@]}"
 do
     case $opt in
         "Install Docker")
-            # Install Docker and Docker Compose
+            #docker + compose
             touch $HOME/.bash_profile
-            cd $HOME
-            if ! command -v docker &> /dev/null; then
-                echo "Installing Docker..."
-                sudo apt update
-                sudo apt upgrade -y
-                sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
-                . /etc/*-release
-                wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-                echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-                sudo apt update
-                sudo apt install docker-ce docker-ce-cli containerd.io -y
-            fi
-
-            if ! docker compose version &> /dev/null; then
-                echo "Installing Docker Compose..."
-                sudo apt install wget jq -y
-                docker_compose_version=$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
-                sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-$(uname -s)-$(uname -m)"
-                sudo chmod +x /usr/bin/docker-compose
-            fi
-            echo "Docker and Docker Compose installed successfully."
+                cd $HOME
+                if ! docker --version; then
+                    sudo apt update
+                    sudo apt upgrade -y
+                    sudo apt install curl jq apt-transport-https ca-certificates gnupg lsb-release -y
+                    . /etc/*-release
+                    wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+                    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                    sudo apt update
+                    sudo apt install docker-ce docker-ce-cli containerd.io -y
+                    docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
+                    sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
+                fi
+                if ! docker compose version; then
+                    sudo apt update
+                    sudo apt upgrade -y
+                    sudo apt install wget jq -y
+                    local docker_compose_version=`wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name"`
+                    sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
+                    sudo chmod +x /usr/bin/docker-compose
+                    . $HOME/.bash_profile
+                fi
             break
             ;;
 
